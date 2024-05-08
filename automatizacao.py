c17806b0fb7c82aa1c8466a9cd3dd8ad032031d0
Domingos de Jesus Alcantara <45907626+DomingosAlcantara@users.noreply.github.com>
2024-05-07 21:32:05 -0300

    Alteracoes do dia 07_05_24
    Algumas correções realizadas neste dia

--- automatizacao.py
+++ automatizacao.py
@@ -2,20 +2,25 @@
 # from openpyxl import load_workbook
 import json
 from pathlib import Path
-# from time import sleep
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from opcoes import Opcoes
 
 
 class Automatizacao ():
     """_summary_
     """
 
-    def __init__(self, navegador: webdriver.Chrome) -> None:
+    def __init__(self, navegador: webdriver.Chrome,
+                 tempo_de_espera: int = 5) -> None:
         self._navegador = navegador
         self._credenciais = None
+        self._tempo_de_espera = tempo_de_espera
 
     def definir_credenciais(self, path_credenciais: Path) -> dict[str, str]:
         """Método para carregar os dados de login
@@ -56,15 +61,46 @@ class Automatizacao ():
         Args:
             url (str): url que deverá ser aberto
         """
+        self._navegador.maximize_window()
         self._navegador.get(url)
 
-    def acessar_opcao_visualizacao(self,)
+    def elemento_visivel(self, elemento: str) -> bool:
+        """_summary_
+
+        Args:
+            elemento (str): _description_
+
+        Returns:
+            bool: _description_
+        """
+        element = WebDriverWait(self._navegador, 60).until(
+            EC.visibility_of_element_located((By.ID, elemento))
+        )
+
+        return element.is_displayed()
+
+    def acessar_opcao_visualizacao(self, opcao: str) -> None:
+        """_summary_
+
+        Args:
+            opcao (str): _description_
+        """
+        sleep(self._tempo_de_espera)
+        try:
+            if self.elemento_visivel(opcao):
+                print("Elemento encontrado")
+                self._navegador.find_element(By.ID, opcao).click()
+        except TimeoutError:
+            print(f"O Item {opcao} não pode ser alcançado")
 
 
 if __name__ == "__main__":
     ERP = "https://erp.correios.com.br"
     credencials_path = Path(__file__).parent  # .home() / "Credenciais"
+    opcoes_auto = Opcoes()
 
     automatizacao = Automatizacao(webdriver.Chrome())
     automatizacao.abrir_url(ERP)
     automatizacao.logar(automatizacao.definir_credenciais(credencials_path))
+    automatizacao.acessar_opcao_visualizacao(opcoes_auto.visualizarTXT)
+    sleep(20)
